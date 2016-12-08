fd7075cf4106ad33b71d6d4c55f59b07c26e93b4
Edmund Chong <7440820@gmail.com>
2016-12-09 10:32:12 +1100

    reassignmentCheck.py : checks if a PERL variable has been re-assgined within the same scop. Used to refactor globals

---
+++ reassignmentCheck.py
@@ -0,0 +1,66 @@
+# checks for re-assginment within the same scope
+
+import os;
+import re;
+
+progName = os.path.basename(__file__);
+print "progName = " + progName;
+curDir = os.path.dirname(__file__);
+relPath = "/folderPath.txt";
+curDir = curDir.replace("\\", "/");
+# folder path holds the fullpath to the folder to be search eg. c://tier1/tier2/
+folderPath = open(curDir+relPath, "r");
+print "Filename:";
+fileName = raw_input();
+folderPath = folderPath.read()
+filePath = folderPath + fileName;
+
+# main loop. Refactor to main() if free.
+while (True):
+    print "After line num:"
+    startLine = 0;
+    endLine = 0;
+    try:
+        startLine = int(raw_input());
+    except:
+        startLine = 1;
+    print "End line num (0 to skip):";
+    try:
+        endLine = int(raw_input());
+    except:
+        endLine = 0;
+    print "Varname:";
+    varName = raw_input();
+    print "Searching " + varName + " in " + filePath;
+    lineNum = [];
+    lineText = [];
+    # we are assuming you're a good programmer that you my or our your PERL variables
+    # we are only checking for re-assignments varName = newValue;
+    regex = re.compile('(\$|@|%)'+varName+'(\,|\))?\s*(\$.*\)*)*=');
+    with open(filePath, "r") as fileHandler:
+        for line_i, line in enumerate(fileHandler, 1):
+            if (regex.search(line)) and line_i >= int(startLine):
+                if endLine < 1:
+                    lineNum.append(str(line_i));
+                    lineText.append(line);
+                else:
+                    if line_i > endLine:
+                        continue;
+                    else:
+                        lineNum.append(str(line_i));
+                        lineText.append(line);
+        fileHandler.close();
+    print "line num\tline text";
+    for index in range(len(lineNum)):            
+        print lineNum[index] + " | " + lineText[index].strip();
+    
+    print "New file?(1 or 0):";
+    newFile = 0;
+    try:
+        newFile = int(raw_input());
+    except:
+        newFile = 0;
+    if newFile > 0:
+        print "Filename:";
+        fileName = raw_input();
+        filePath = folderPath + fileName;
