35d42f03ee3e160f66ce89bd9f4a43d4866e55bf
Edmund Chong <7440820@gmail.com>
2016-12-09 15:55:09 +1100

    PERLdeclarationFinder.py : Made this more user friendly, only ask for new scope line boundaries if flag is set

--- PERLdeclarationFinder.py
+++ PERLdeclarationFinder.py
@@ -15,19 +15,24 @@ print "Filename:";
 fileName = raw_input();
 folderPath = folderPath.read()
 filePath = folderPath + fileName;
+originalFilePath = 0;
+resetLineNum = 1;
+startLine = 0;
+endLine = 0;
 while (True):
-    print "After line num:"
-    startLine = 0;
-    endLine = 0;
-    try:
-        startLine = int(raw_input());
-    except:
-        startLine = 1;
-    print "End line num (0 to skip):";
-    try:
-        endLine = int(raw_input());
-    except:
+    if (resetLineNum):
+        print "After line num:"
+        startLine = 0;
         endLine = 0;
+        try:
+            startLine = int(raw_input());
+        except:
+            startLine = 1;
+        print "End line num (0 to skip):";
+        try:
+            endLine = int(raw_input());
+        except:
+            endLine = 0;
     print "Varname:";
     varName = raw_input();
     print "Searching " + varName + " in " + filePath;
@@ -44,43 +49,55 @@ while (True):
     regex3 = re.compile('my.*(\$|@|%)'+varName+'\)*;');
     # LHS check
     regex4 = re.compile('my.*(\$|@|%)'+varName+'[,\)\s*]');
-    with open(filePath, "r") as fileHandler:
-        for line_i, line in enumerate(fileHandler, 1):
-            assgn = line.split('=');
-            assgnChk = 0;
-            # it's an assign, check LHS only
-            if len(assgn) > 1:
-                line = assgn[0];
-                assgnChk = regex4.search(line);
-            if (regex.search(line) or regex2.search(line) or regex3.search(line) or assgnChk) and line_i >= int(startLine):
-                if endLine < 1:
+    try:
+        fileHandler = open(filePath, "r");
+    except:
+        print "Failed to open file. Using last provided file: " + originalFilePath;
+        filePath = originalFilePath;
+        fileHandler = open(filePath, "r");
+    for line_i, line in enumerate(fileHandler, 1):
+        assgn = line.split('=');
+        assgnChk = 0;
+        # it's an assign, check LHS only
+        if len(assgn) > 1:
+            line = assgn[0];
+            assgnChk = regex4.search(line);
+        if (regex.search(line) or regex2.search(line) or regex3.search(line) or assgnChk) and line_i >= int(startLine):
+            if endLine < 1:
+                if len(assgn) > 1:
+                    lineNum.append(str(line_i));
+                    lineText.append(assgn[0] + " = " + assgn[1]);
+                else:
+                    lineNum.append(str(line_i));
+                    lineText.append(line);
+            else:
+                if line_i > endLine:
+                    continue;
+                else:
                     if len(assgn) > 1:
                         lineNum.append(str(line_i));
                         lineText.append(assgn[0] + " = " + assgn[1]);
                     else:
                         lineNum.append(str(line_i));
                         lineText.append(line);
-                else:
-                    if line_i > endLine:
-                        continue;
-                    else:
-                        if len(assgn) > 1:
-                            lineNum.append(str(line_i));
-                            lineText.append(assgn[0] + " = " + assgn[1]);
-                        else:
-                            lineNum.append(str(line_i));
-                            lineText.append(line);
-        fileHandler.close();
+    fileHandler.close();
     print "line num\tline text";
     for index in range(len(lineNum)):            
         print lineNum[index] + " | " + lineText[index].strip();
     
     print "New file?(1 or 0):";
     newFile = 0;
+    # if we got here, everything has been working perfectly
+    originalFilePath = filePath;
     try:
         newFile = int(raw_input());
     except:
         newFile = 0;
+    print "Reset scope?(1 or 0):";
+    try:
+        resetLineNum = int(raw_input());
+    except:
+        resetLineNum = 0;
     if newFile > 0:
         print "Filename:";
         fileName = raw_input();
